3182166862536e130749df6dccb6c040308a2b5d
D4fra <148436310+D4fra@users.noreply.github.com>
2023-11-14 11:19:22 +0100

    Initial Commit

---
+++ NC1_feltrinelli/NC1_feltrinelliApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_feltrinelliApp.swift
+//  NC1_feltrinelli
+//
+//  Created by Davide Formisano on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NC1_feltrinelliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
